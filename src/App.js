ba7a6c408aac2953aa5ddeb62b6f9dbab287b96e
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-11 22:05:40 -0600

    button and typography examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { Button, Typography } from "@mui/material";
 
+import { Add, Settings } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Button
+        variant="text"
+      >
+        Text
+      </Button>
+      <Button
+      startIcon={<Settings />}
+        variant="text"
+      >Settings</Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >Add New Post</Button>
+      <Button
+        variant="outlined"
+        disabled
+      >Outlined</Button>
+      <Typography
+        variant="h1"
+        component="p"
+      >
+        It uses h1 style but it's a p tag
+      </Typography>
+      <Button
+        variant="contained"
+        disabled
+        sx={{
+          backgroundColor:"skyblue",
+          color:"#888",
+          margin:5,
+          "&:hover":{
+            backgroundColor:"lightblue"
+          },
+          "&:disabled":{
+            backgroundColor:"gray",
+            color:"white"
+          }
+        }}
+      >My Unique Button</Button>
     </div>
   );
 }
