1c88c8831cbec96ba7edacdf547b55443a5122cf
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-11 23:10:15 -0600

    responsive nav added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Sidebar from './components/Sidebar';
 import Rightbar from './components/Rightbar';
 import Feed from './components/Feed';
 import { Box, Stack } from '@mui/material';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <Box>
-      {/* Navbar */}
+      <Navbar />
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Feed />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import { Pets, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const IconContainer = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  }
+}));
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography
+          variant="h6"
+          sx={{
+            display: {
+              xs: "none",
+              sm: "block",
+            },
+          }}
+        >
+          My Blog
+        </Typography>
+        <Pets
+          sx={{
+            display: {
+              xs: "block",
+              sm: "none",
+            },
+          }}
+        />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <IconContainer>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{
+              width: 30,
+              height: 30,
+            }}
+            src="https://fastly.picsum.photos/id/57/2448/3264.jpg?hmac=ewraXYesC6HuSEAJsg3Q80bXd1GyJTxekI05Xt9YjfQ"
+          />
+        </IconContainer>
+        <UserBox>
+          <Avatar
+            sx={{
+              width: 30,
+              height: 30,
+            }}
+            src="https://fastly.picsum.photos/id/57/2448/3264.jpg?hmac=ewraXYesC6HuSEAJsg3Q80bXd1GyJTxekI05Xt9YjfQ"
+          />
+          <Typography
+            variant="span"
+          >John Doe</Typography>
+        </UserBox>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
