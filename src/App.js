90aa05a3850685ccf6cc0188d3befabc10e62eeb
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-11 22:36:56 -0600

    Stack used as flex layout to create Sidebar, Feed and Rightbar sections

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
-import { Button, Typography } from "@mui/material";
-import { Add, Settings } from '@mui/icons-material';
-import { styled } from '@mui/material/styles';
+import Sidebar from './components/Sidebar';
+import Rightbar from './components/Rightbar';
+import Feed from './components/Feed';
+import { Box, Stack } from '@mui/material';
 
 function App() {
 
-  const BlueButton = styled(Button)(({theme}) => ({
-    backgroundColor: theme.palette.otherColor.main,
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue"
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white"
-    }
-  }));
-
-
   return (
-    <div className="App">
-      <Button
-        variant="text"
-      >
-        Text
-      </Button>
-      <Button
-        startIcon={<Settings />}
-        variant="contained"
-        color="otherColor"
-      >Settings</Button>
-      <Button
-        startIcon={<Add />}
-        variant="contained"
-        color="success"
-        size="small"
-      >Add New Post</Button>
-      <Button
-        variant="outlined"
-        disabled
-      >Outlined</Button>
-      <Typography
-        variant="h1"
-        component="p"
-      >
-        It uses h1 style but it's a p tag
-      </Typography>
-      <BlueButton>My Blue Button</BlueButton>
-      <BlueButton>Another Button</BlueButton>
-    </div>
+    <Box>
+      {/* Navbar */}
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
