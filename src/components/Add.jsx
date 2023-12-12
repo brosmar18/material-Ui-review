ba161822453666d5bc3fd1eb5f686730be3d7c03
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-12 10:07:08 -0600

    Add modal created

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,38 @@
-import { Fab, Tooltip } from "@mui/material";
-import { Add as AddIcon } from "@mui/icons-material";
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { Add as AddIcon, EmojiEmotions } from "@mui/icons-material";
+import ImageIcon from "@mui/icons-material/Image";
+import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+const StyleModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 
 const Add = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Tooltip
@@ -12,11 +42,67 @@ const Add = () => {
           bottom: 20,
           left: { xs: "calc(50% - 25px)", md: 30 },
         }}
+        onClick={(e) => setOpen(true)}
       >
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyleModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://picsum.photos/seed/picsum/200/300"
+              sx={{
+                width: 30,
+                height: 30,
+              }}
+            />
+            <Typography fontWeight={500} variant="span">
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{
+              width: "100%",
+            }}
+            multiline
+            rows={4}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack
+            direction="row"
+            justifyContent={"space-between"}
+            gap={1}
+            mt={2}
+            mb={3}
+          >
+            <EmojiEmotions color="primary" />
+            <ImageIcon color="secondary" />
+            <VideoCameraBackIcon color="success" />
+            <PersonAddIcon color="error" />
+          </Stack>
+          <ButtonGroup fullWidth variant="contained">
+            <Button>Post</Button>
+            <Button
+              sx={{
+                width: "100px",
+              }}
+            >
+              <DateRangeIcon />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyleModal>
     </>
   );
 };
